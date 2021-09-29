c873422c144a26c9fa81aab4bcdc372985e017de
Suraj Das <blogwapz@gmail.com>
2021-09-29 22:14:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './public/**/*.js',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
